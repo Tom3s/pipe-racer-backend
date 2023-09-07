d980b5b3d41afe945f1f88d3a184dcf30f854e56
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-09-07 12:45:35 +0300

    User Model

---
+++ src/Models/User.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document } from "mongoose";
+
+const { Schema } = mongoose;
+
+export interface IUser extends Document {
+	username: string;
+	nickname?: string;
+	email?: string;
+	passwordHash?: string;
+	profilePictureFileName?: string;
+	admin: boolean;
+}
+
+export const UserSchema = new Schema({
+	username: { type: String, required: true, unique: true },
+	nickname: String,
+	email: String,
+	passwordHash: String,
+	profilePictureFileName: String,
+	admin: { type: Boolean, required: true, default: false }
+});
+
+export const User = mongoose.model<IUser>("User", UserSchema);
