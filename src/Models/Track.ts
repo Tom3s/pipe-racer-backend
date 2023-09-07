5573fd6f69e7a20a36a32b75ceacb62db4efce75
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-09-07 12:53:05 +0300

    Track Model

---
+++ src/Models/Track.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Types } from "mongoose";
+import { IUser, User } from "./User";
+
+const { Schema } = mongoose;
+
+export interface ITrack extends Document {
+	name: string;
+	trackFileName: string;
+	trackImageFileName?: string;
+	author: Types.ObjectId | IUser;
+	rating: number;
+	downloads: number;
+}
+
+export const TrackSchema = new Schema({
+	name: { type: String, required: true },
+	trackFileName: { type: String, required: true },
+	trackImageFileName: String,
+	author: { 
+		type: Schema.Types.ObjectId, 
+		ref: "User", 
+		required: true,
+		validate: {
+			validator: (userId: Types.ObjectId) => {
+				return User.exists({ _id: userId });
+			},
+			message: "Invalid user ID"
+		}
+	},
+	rating: { type: Number, required: true, default: 0 },
+	downloads: { type: Number, required: true, default: 0 }
+});
+
+export const Track = mongoose.model<ITrack>("Track", TrackSchema);
