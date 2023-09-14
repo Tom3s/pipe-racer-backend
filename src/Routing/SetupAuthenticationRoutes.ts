b0f39fcfe1c024ad73a8035c80d60f2c45b66791
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-09-14 18:24:04 +0300

    Email reg confirm link

--- src/Routing/SetupAuthenticationRoutes.ts
+++ src/Routing/SetupAuthenticationRoutes.ts
@@ -6,13 +6,44 @@ import { sendErrorResponse, sendOKResponse } from '../Global/ResponseHandler';
 export const setupAuthenticationRoutes = (app: Express, authService: AuthenticationService) => {
 	const basePath = '/api/auth';
 
+	function sendEmail(tokenUrl: string, email: string) {
+		let emailText = "Please confirm your registration by clicking on the following link: \n" + tokenUrl;
+		emailText += "\n\nThis link will expire in 24 hours.";
+
+		const nodemailer = require('nodemailer');
+
+		const transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: {
+				user: process.env.EMAIL_ADDRESS,
+				pass: process.env.EMAIL_PASSWORD
+			}
+		});
+
+		const mailOptions = {
+			from: process.env.EMAIL_ADDRESS,
+			to: email,
+			subject: 'Registration confirmation for Pipe Racer',
+			text: emailText
+		};
+
+		transporter.sendMail(mailOptions, function (error: any, info: any) {
+			if (error) {
+				throw error;
+			} else {
+				console.log('Email sent: ' + info.response);
+			}
+		});
+	}
+
 	app.post(`${basePath}/register`, async (request: Request, response: Response) => {
 		const username = request.body.username;
 		const password = request.body.password;
 		const email = request.body.email;
 		authService.getRegistrationToken(username, password, email)
 			.then((tokenUrl) => {
-				sendOKResponse(response, tokenUrl);
+				sendEmail(tokenUrl, email);
+				sendOKResponse(response, "Check your email for a confirmation link.");
 			}
 			).catch((error) => {
 				sendErrorResponse(response, error);
