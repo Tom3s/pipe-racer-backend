3790601ddf2f7fec1486873ba3ad05e5a5a3bd7d
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-09-20 23:24:17 +0300

    Update PasswordTooWeakError.ts

--- src/Errors/PasswordTooWeakError.ts
+++ src/Errors/PasswordTooWeakError.ts
@@ -4,7 +4,7 @@ import { ResponseError } from "../Models/ResponseError";
 export class PassworTooWeakError extends ResponseError {
 	statusCode = StatusCodes.UNPROCESSABLE_ENTITY;
 	constructor() {
-		super("Password is too weak.");
+		super("Password must be at least 8 characters long and contain at least one uppercase, one lowercase, and one special character/number.");
 	}
 }
 	
