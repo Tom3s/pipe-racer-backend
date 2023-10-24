b2ccbc94e8e868dfe2851715ee4fcf3d6ab5c8fd
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-10-24 22:29:06 +0300

    Update connectToDatabase.ts

--- src/connectToDatabase.ts
+++ src/connectToDatabase.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
     try {
-        const conn = await mongoose.connect('mongodb://localhost:27017/pipe-racer' );
+        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/pipe-racer' );
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error: any) {
         console.error(`Error: ${error.message}`);
